ae021496d9ca713a8a93c79db92fab355ad9f831
JordiMV10 <jordimv10@hotmail.com>
2020-01-21 13:17:23 +0100

    Generada BBDD, arranca web y llama al controller Students y le inserta 2 objetos.

---
+++ WebAspNetAcademy/App/Bootstrapper.cs
@@ -0,0 +1,52 @@
+﻿using Academy.Lib.DAL;
+using Academy.Lib.DAL.Repositories;
+using Academy.Lib.Models;
+using Academy.Lib.Repositories;
+using Common.Lib.Core;
+using Common.Lib.Core.Context;
+using Common.Lib.DAL.EFCore;
+using Common.Lib.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAspNetAcademy.DbContextFactory;
+
+namespace WebAspNetAcademy.App
+{
+    public class Bootstrapper
+    {
+        
+        public Bootstrapper()
+        {
+
+        }
+
+
+        public void Init(IDependencyContainer dp, Func<DbContext> dbContextConst)
+        {
+            RegisterRepositories(dp, dbContextConst);
+        }
+
+        void RegisterRepositories(IDependencyContainer dp, Func<DbContext> dbContextConst)
+        {
+            var studentRepoBuilder = new Func<object[], object>((parameters) =>
+            {
+                return new EfCoreRepository<Student>(dbContextConst());
+            });
+            //var subjectsRepoBuilder = new Func<object[], object>((parameters) =>
+            //{
+            //    return new SubjectsRepository(dbContextConst());
+            //});
+
+            dp.Register<IRepository<Student>, EfCoreRepository<Student>>(studentRepoBuilder);
+
+            //dp.Register<IRepository<Subject>, SubjectsRepository>(subjectsRepoBuilder);
+            //dp.Register<ISubjectsRepository, SubjectsRepository>(subjectsRepoBuilder);
+        }
+
+    }
+
+}
+
--- WebAspNetAcademy/Startup.cs
+++ WebAspNetAcademy/Startup.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Academy.Lib.DAL;
+using Common.Lib.Core;
+using Common.Lib.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebAspNetAcademy.App;
 
 namespace WebAspNetAcademy
 {
@@ -14,8 +20,66 @@ namespace WebAspNetAcademy
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        //public void ConfigureServices(IServiceCollection services)
+        //{
+        //}
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        //{
+        //    if (env.IsDevelopment())
+        //    {
+        //        app.UseDeveloperExceptionPage();
+        //    }
+
+        //    app.UseRouting();
+
+        //    app.UseEndpoints(endpoints =>
+        //    {
+        //        endpoints.MapGet("/", async context =>
+        //        {
+        //            await context.Response.WriteAsync("Hello HOLAA World!");
+        //        });
+        //    });
+        //}
+
+
+        //Nou startup
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            var dbConnection = Configuration.GetConnectionString("AcademyDbConnection");
+            services.AddDbContext<AcademyDbContext>(options => options.UseSqlite(dbConnection, b => b.MigrationsAssembly("WebAspNetAcademy")));
+
+            var bootstrapper = new Bootstrapper();
+            Entity.DepCon = new SimpleDependencyContainer();
+
+            bootstrapper.Init(Entity.DepCon, GetDbConstructor(dbConnection));  //original
+            //bootstrapper.Init();   //MEU
+
+
+        }
+
+        private static Func<AcademyDbContext> GetDbConstructor(string dbConnection)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AcademyDbContext>();
+            optionsBuilder.UseSqlite(dbConnection, b => b.MigrationsAssembly("WebAspNetAcademy"));
+
+            var dbContextConst = new Func<AcademyDbContext>(() =>
+            {
+                return new AcademyDbContext(optionsBuilder.Options);
+            });
+            return dbContextConst;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +90,20 @@ namespace WebAspNetAcademy
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello HOLAA World!");
-                });
+                endpoints.MapControllers();
             });
         }
+
+
+
+
     }
 }
