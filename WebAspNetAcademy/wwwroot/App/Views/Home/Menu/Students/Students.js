2467ada840463209bf7d52a00887ec2c0bb35715
JordiMV10 <jordimv10@hotmail.com>
2020-02-02 19:51:27 +0100

    Ultimos cambios

---
+++ WebAspNetAcademy/wwwroot/App/Views/Home/Menu/Students/Students.js
@@ -0,0 +1,52 @@
+﻿class Students {
+
+    get Students() {
+        return this._students;
+    }
+    set Students(value) {
+        this._students = value;
+    }
+
+    get IsLogon() {
+        return Globals.IsLogon;
+    }
+
+    constructor($http) {
+        this._students = [];
+        this.Http = $http;
+    }
+
+    GetStudents() {
+        this.Http.get("/api/students").then(
+            (response) => {
+                this.Students.length = 0;
+                for (let i in response.data) {
+                    this.Students.push(response.data[i]);
+                }
+            },
+            function errorCallback(response) {
+                console.log("POST-ing of data failed");
+            }
+        );
+    }
+
+
+
+    //Meu
+
+    //GoMenu() {
+    //    this.Http.get("/app/views/home/menu/menu").then(
+    //        (response) => {
+
+    //            if (response.data === true)
+    //                Globals.IsLogon = true;
+    //        },
+    //        function errorCallback(response) {
+    //            console.log("POST-ing of data failed");
+    //        }
+    //    );
+    //}
+
+
+
+}
